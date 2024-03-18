7750ada8018fe0a971789b0f820a31804b318c56
nanawaredivya <divyananaware28@gmail.com>
2024-03-18 19:01:10 +0530

    add Resume page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Button, Typography } from '@mui/material';
 
 const Resume = () => {
-  return (
-    <div>Resume</div>
-  )
-}
+  const downloadResume = () => {
+    const link = document.createElement('a');
+    link.href = '/Doc/Divya-React-Frontend-Resume.docx';
+    link.download = 'resume.doc';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link); // Remove the link from the DOM after download
+  };
 
-export default Resume
+  return (
+    <div 
+    style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        minHeight: '70vh',  
+        backgroundColor: '#e8e8e8',
+        }}>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <Typography variant="h6" gutterBottom>
+        Download Resume
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Click the button below to download my resume in DOC format.
+      </Typography>
+      <Button variant="contained" onClick={downloadResume}>
+        Download
+      </Button>
+    </div>
+    </div>
+  );
+};
+
+export default Resume;
